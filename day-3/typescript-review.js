c2965ded4d68c32f4e49b38e598e3a2cb53413eb
MohamedA <mohammedmon3m@yahoo.com>
2021-08-18 13:47:34 -0400

    typescript-review file

---
+++ day-3/typescript-review.js
@@ -0,0 +1,2 @@
+var SUNNY_DAY_MESSAGE = "It is sunny today!";
+console.log(SUNNY_DAY_MESSAGE);
---
+++ day-3/typescript-review.ts
@@ -0,0 +1,2 @@
+const SUNNY_DAY_MESSAGE: string="It is sunny today!";
+console.log(SUNNY_DAY_MESSAGE);
